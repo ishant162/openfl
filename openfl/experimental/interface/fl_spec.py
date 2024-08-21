d22e1c3a6e5b2b1e76f4669a0cd6d17e9292f222
Ishant Thakare <ishantrog752@gmail.com>
2024-08-21 13:21:06 +0530

    Resolving merge conflicts
    Signed-off-by: Ishant Thakare <ishantrog752@gmail.com>

--- openfl/experimental/interface/fl_spec.py
+++ openfl/experimental/interface/fl_spec.py
@@ -1,18 +1,15 @@
-# Copyright (C) 2020-2023 Intel Corporation
+# Copyright 2020-2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
+
+
 """openfl.experimental.interface.flspec module."""
 
 from __future__ import annotations
 
 import inspect
 from copy import deepcopy
-from typing import Callable, List, Type, Union
+from typing import Callable, List, Type
 
-from typing import TYPE_CHECKING
-if TYPE_CHECKING:
-	from openfl.experimental.runtime import Runtime
-	from openfl.experimental.runtime import LocalRuntime
-	from openfl.experimental.runtime import FederatedRuntime
 from openfl.experimental.utilities import (
     MetaflowInterface,
     SerializationError,
@@ -30,31 +27,47 @@ class FLSpec:
     _initial_state = None
 
     def __init__(self, checkpoint: bool = False):
+        """Initializes the FLSpec object.
+
+        Args:
+            checkpoint (bool, optional): Determines whether to checkpoint or
+                not. Defaults to False.
+        """
         self._foreach_methods = []
         self._checkpoint = checkpoint
 
     @classmethod
     def _create_clones(cls, instance: Type[FLSpec], names: List[str]) -> None:
-        """Creates clones for instance for each collaborator in names"""
+        """Creates clones for instance for each collaborator in names.
+
+        Args:
+            instance (Type[FLSpec]): The instance to be cloned.
+            names (List[str]): The list of names for the clones.
+        """
         cls._clones = {name: deepcopy(instance) for name in names}
 
     @classmethod
     def _reset_clones(cls):
-        """Reset clones"""
-        cls._clones = {}
+        """Resets the clones of the class."""
+
+        cls._clones = []
 
     @classmethod
     def save_initial_state(cls, instance: Type[FLSpec]) -> None:
-        """Save initial state of instance before executing the flow"""
+        """Saves the initial state of an instance before executing the flow.
+
+        Args:
+            instance (Type[FLSpec]): The instance whose initial state is to be
+                saved.
+        """
         cls._initial_state = deepcopy(instance)
 
     def run(self) -> None:
-        """Starts the execution of the flow"""
+        """Starts the execution of the flow."""
+
         # Submit flow to Runtime
         if str(self._runtime) == "LocalRuntime":
-            self._metaflow_interface = MetaflowInterface(
-                self.__class__, self.runtime.backend
-            )
+            self._metaflow_interface = MetaflowInterface(self.__class__, self.runtime.backend)
             self._run_id = self._metaflow_interface.create_run()
             # Initialize aggregator private attributes
             self.runtime.initialize_aggregator()
@@ -68,7 +81,8 @@ class FLSpec:
             try:
                 # Execute all Participant (Aggregator & Collaborator) tasks and
                 # retrieve the final attributes
-                # start step is the first task & invoked on aggregator through runtime.execute_task
+                # start step is the first task & invoked on aggregator through
+                # runtime.execute_task
                 final_attributes = self.runtime.execute_task(
                     self,
                     self.start,
@@ -91,47 +105,43 @@ class FLSpec:
             for name, attr in final_attributes:
                 setattr(self, name, attr)
         elif str(self._runtime) == "FederatedRuntime":
-            self.prepare_workspace_archive()
-            self.deploy_workspace()
+            pass
         else:
             raise Exception("Runtime not implemented")
 
     @property
-    def runtime(self) -> Type[Union[LocalRuntime, FederatedRuntime]]:
-        """Returns flow runtime"""
+    def runtime(self):
+        """Returns flow runtime.
+
+        Returns:
+            Type[Runtime]: The runtime of the flow.
+        """
         return self._runtime
 
     @runtime.setter
-    def runtime(self, runtime: Type[Runtime]) -> None:
-        """Sets flow runtime"""
+    def runtime(self, runtime) -> None:
+        """Sets flow runtime.
+
+        Args:
+            runtime (Type[Runtime]): The runtime to be set.
+
+        Raises:
+            TypeError: If the provided runtime is not a valid OpenFL Runtime.
+        """
+        if str(runtime) not in ["LocalRuntime", "FederatedRuntime"]:
+            raise TypeError(f"{runtime} is not a valid OpenFL Runtime")
         self._runtime = runtime
 
-    def prepare_workspace_archive(self) -> None:
-        # This will extract private attrs and make plan.yaml and data.yaml
-        self.runtime.prepare_workspace_archive()
-
-    def deploy_workspace(self) -> bool:
-        # Use runtime object to send experiment.zip to director
-        return self.runtime.submit_workspace()
-
-    def stream_metrics(self) -> None:
-        # This has to work for agg based and director based workflow
-        # prints metrics on the console.
-        self.runtime.stream_metrics()
-
-    def experiment_status(self) -> int:
-        # Aggregator will report experiment status to Director,
-        # which will send it here and from here to user.
-        return self.runtime.get_experiment_status()
-
     def _capture_instance_snapshot(self, kwargs):
-        """
-        Takes backup of self before exclude or include filtering
+        """Takes backup of self before exclude or include filtering.
 
         Args:
             kwargs: Key word arguments originally passed to the next function.
                     If include or exclude are in the kwargs, the state of the
-                    aggregator needs to be retained
+                    aggregator needs to be retained.
+
+        Returns:
+            return_objs (list): A list of return objects.
         """
         return_objs = []
         if "exclude" in kwargs or "include" in kwargs:
@@ -139,32 +149,34 @@ class FLSpec:
             return_objs.append(backup)
         return return_objs
 
-    def _is_at_transition_point(
-        self, f: Callable, parent_func: Callable
-    ) -> bool:
+    def _is_at_transition_point(self, f: Callable, parent_func: Callable) -> bool:
         """
-        Has the collaborator finished its current sequence?
+        Determines if the collaborator has finished its current sequence.
 
         Args:
-            f:           The next function to be executed
-            parent_func: The previous function executed
+            f (Callable): The next function to be executed.
+            parent_func (Callable): The previous function executed.
+
+        Returns:
+            bool: True if the collaborator has finished its current sequence,
+                False otherwise.
         """
         if parent_func.__name__ in self._foreach_methods:
             self._foreach_methods.append(f.__name__)
             if should_transfer(f, parent_func):
-                print(
-                    f"Should transfer from {parent_func.__name__} to {f.__name__}"
-                )
+                print(f"Should transfer from {parent_func.__name__} to {f.__name__}")
                 self.execute_next = f.__name__
                 return True
         return False
 
-    def _display_transition_logs(
-        self, f: Callable, parent_func: Callable
-    ) -> None:
+    def _display_transition_logs(self, f: Callable, parent_func: Callable) -> None:
         """
-        Prints aggregator to collaborators or
-        collaborators to aggregator state transition logs
+        Prints aggregator to collaborators or collaborators to aggregator
+        state transition logs.
+
+        Args:
+            f (Callable): The next function to be executed.
+            parent_func (Callable): The previous function executed.
         """
         if aggregator_to_collaborator(f, parent_func):
             print("Sending state from aggregator to collaborators")
@@ -173,31 +185,43 @@ class FLSpec:
             print("Sending state from collaborator to aggregator")
 
     def filter_exclude_include(self, f, **kwargs):
-        """
-        This function filters exclude/include attributes
+        """Filters exclude/include attributes for a given task within the flow.
 
         Args:
-            flspec_obj  :  Reference to the FLSpec (flow) object
-            f           :  The task to be executed within the flow
+            f (Callable): The task to be executed within the flow.
+            **kwargs (dict): Additional keyword arguments. These should
+                include:
+                - "foreach" (str): The attribute name that contains the list
+                of selected collaborators.
+                - "exclude" (list, optional): List of attribute names to
+                exclude. If an attribute name is present in this list and the
+                clone has this attribute, it will be filtered out.
+                - "include" (list, optional): List of attribute names to
+                include. If an attribute name is present in this list and the
+                clone has this attribute, it will be included.
         """
         selected_collaborators = getattr(self, kwargs["foreach"])
 
         for col in selected_collaborators:
             clone = FLSpec._clones[col]
             clone.input = col
-            if (
-                "exclude" in kwargs and hasattr(clone, kwargs["exclude"][0])
-            ) or ("include" in kwargs and hasattr(clone, kwargs["include"][0])):
+            if ("exclude" in kwargs and hasattr(clone, kwargs["exclude"][0])) or (
+                "include" in kwargs and hasattr(clone, kwargs["include"][0])
+            ):
                 filter_attributes(clone, f, **kwargs)
             artifacts_iter, _ = generate_artifacts(ctx=self)
             for name, attr in artifacts_iter():
                 setattr(clone, name, deepcopy(attr))
             clone._foreach_methods = self._foreach_methods
 
-    def restore_instance_snapshot(
-        self, ctx: FLSpec, instance_snapshot: List[FLSpec]
-    ):
-        """Restores attributes from backup (in instance snapshot) to ctx"""
+    def restore_instance_snapshot(self, ctx: FLSpec, instance_snapshot: List[FLSpec]):
+        """Restores attributes from backup (in instance snapshot) to ctx.
+
+        Args:
+            ctx (FLSpec): The context to restore the attributes to.
+            instance_snapshot (List[FLSpec]): The list of FLSpec instances
+                that serve as the backup.
+        """
         for backup in instance_snapshot:
             artifacts_iter, _ = generate_artifacts(ctx=backup)
             for name, attr in artifacts_iter():
@@ -205,9 +229,7 @@ class FLSpec:
                     setattr(ctx, name, attr)
 
     def get_clones(self, kwargs):
-        """
-        Create, and prepare clones
-        """
+        """Create, and prepare clones."""
         FLSpec._reset_clones()
         FLSpec._create_clones(self, self.runtime.collaborators)
         selected_collaborators = self.__getattribute__(kwargs["foreach"])
@@ -223,8 +245,11 @@ class FLSpec:
             clone._metaflow_interface = self._metaflow_interface
 
     def next(self, f, **kwargs):
-        """
-        Next task in the flow to execute
+        """Specifies the next task in the flow to execute.
+
+        Args:
+            f (Callable): The next task that will be executed in the flow.
+            **kwargs: Additional keyword arguments.
         """
         # Get the name and reference to the calling function
         parent = inspect.stack()[1][3]
--- openfl/experimental/runtime/federated_runtime.py
+++ openfl/experimental/runtime/federated_runtime.py
@@ -1,5 +1,7 @@
-# Copyright (C) 2020-2023 Intel Corporation
+# Copyright 2020-2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
+
+
 """ openfl.experimental.runtime package LocalRuntime class."""
 
 from __future__ import annotations
@@ -11,120 +13,78 @@ from openfl.experimental.runtime.runtime import Runtime
 if TYPE_CHECKING:
     from openfl.experimental.interface import Aggregator
     from openfl.experimental.interface import Collaborator
-    from openfl.experimental.interface import Federation
 
-from typing import List, Type, Dict, Any, Union
+from typing import List, Type
 
-
-class ExperimentStatus:
-    SUBMITTED = 0
-    RUNNING = 1
-    ERROR = 2
-    FINISHED = 3
 
 class FederatedRuntime(Runtime):
+    """Class for a federated runtime, derived from the Runtime class.
+
+    Attributes:
+        aggregator (Type[Aggregator]): The aggregator participant.
+        collaborators (List[Type[Collaborator]]): The list of collaborator
+            participants.
+    """
 
     def __init__(
         self,
         aggregator: str = None,
         collaborators: List[str] = None,
-        federation: Type[Federation] = None,
         **kwargs,
     ) -> None:
-        """
-        Use single node to run the flow
+        """Initializes the FederatedRuntime object.
+
+        Use single node to run the flow.
 
         Args:
-            aggregator:    Name of the aggregator.
-            collaborators: List of collaborator names.
-            federation:    Federation class object.
-
-        Returns:
-            None
+            aggregator (str, optional): Name of the aggregator. Defaults to
+                None.
+            collaborators (List[str], optional): List of collaborator names.
+                Defaults to None.
+            **kwargs: Additional keyword arguments.
         """
         super().__init__()
-        self.aggregator = aggregator
-        self.collaborators = collaborators
-        # self.federation = federation
+        if aggregator is not None:
+            self.aggregator = aggregator
+
+        if collaborators is not None:
+            self.collaborators = collaborators
 
     @property
     def aggregator(self) -> str:
-        """Returns name of _aggregator"""
+        """Returns name of _aggregator."""
         return self._aggregator
 
     @aggregator.setter
-    def aggregator(self, aggregator: Type[Aggregator]):
-        """Set LocalRuntime _aggregator"""
-        self._aggregator = aggregator
+    def aggregator(self, aggregator_name: Type[Aggregator]):
+        """Set LocalRuntime _aggregator.
+
+        Args:
+            aggregator_name (Type[Aggregator]): The name of the aggregator to
+                set.
+        """
+        self._aggregator = aggregator_name
 
     @property
     def collaborators(self) -> List[str]:
-        """
-        Return names of collaborators. Don't give direct access to private attributes
+        """Return names of collaborators.
+
+        Don't give direct access to private attributes.
+
+        Returns:
+            List[str]: The names of the collaborators.
         """
         return self.__collaborators
 
     @collaborators.setter
     def collaborators(self, collaborators: List[Type[Collaborator]]):
-        """Set LocalRuntime collaborators"""
+        """Set LocalRuntime collaborators.
+
+        Args:
+            collaborators (List[Type[Collaborator]]): The list of
+                collaborators to set.
+        """
         self.__collaborators = collaborators
-
-    @property
-    def federation(self) -> Type[Federation]:
-        """Returns name of _aggregator"""
-        return self.__federation
-
-    @federation.setter
-    def aggregator(self, federation: Type[Federation]):
-        """Set LocalRuntime _aggregator"""
-        self.__federation = federation
-
-    def prepare_workspace_archive(self) -> None:
-        # self.extract_private_attrs()
-        self.__prepare_plan()
-        self.__prepare_data()
-
-    def extract_private_attrs(self) -> Dict[str, Any]:
-        # This method will call workspace_export module and
-        # extract private attributes from aggregator & collaborator.
-        pass
-
-    def __prepare_plan(
-        self,
-        arguments_required_to_initialize_the_flow,
-        pickled_objects_name,
-    ) -> None:
-        # Prepare plan.yaml
-        # Take plan template from openfl.experimental.interface.cli_helper.WORKSPACE "workspace/plan/plans/default/base_plan_interactive_api.yaml"
-        # parse the plan
-        # Fill the details for the new flow
-        # 
-        pass
-
-    def __prepare_data(self) -> None:
-        # Prepare data.yaml
-        pass
-
-    def remove_workspace_archive(self) -> None:
-        # Delete experiment.zip file
-        pass
-
-    def submit_workspace(self) -> int:
-        # Use federation._dir_client to submit workspace to Director
-        return ExperimentStatus.SUBMITTED
-
-    def stream_metrics(self) -> Dict[str, Union[str, float]]:
-        # Use federation._dir_client object to get metrics and report to user
-        pass
-
-    def remove_experiment_data(self, flow_id: int) -> None:
-        # Use federation._dir_client to remove experiment data including checkpoints
-        pass
-
-    def get_experiment_status(self) -> int:
-        # Use federation._dir_client to comminicate to director to get experiment status
-        # Return int as defined in ExperimentStatus
-        return ExperimentStatus.SUBMITTED
 
     def __repr__(self):
         return "FederatedRuntime"
